e0276e192a0edde83d30ad22a261ca19f54ccc31
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-01-18 22:55:41 +0530

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        int k=9-n;
+        int t=k*(k+1)/2;
+        cout<<t*6<<endl;
+    }
+}
