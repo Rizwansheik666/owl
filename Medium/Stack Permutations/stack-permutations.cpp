01885c9e87c34625383e2e6fbb4855fa47dc46b5
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-01-22 22:38:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stack Permutations/stack-permutations.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        stack<int>st;
+        int i=0,j=0;
+        while(i<N and j<N){
+            st.push(A[i]);
+            while(!st.empty() and st.top()==B[j]){
+                st.pop();
+                j++;
+            }
+            i++;
+        }
+        if(st.empty()){
+            return 1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        Solution ob;
+        cout<<ob.isStackPermutation(n,a,b)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
