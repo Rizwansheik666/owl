cff4351242e37684e1f395e159423389c29db8b9
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-02-11 21:34:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Recamans sequence/recamans-sequence.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> recamanSequence(int n){
+        // code here
+        vector<int> v;
+        map<int,int> m;
+        v.push_back(0);
+        m[0]=1;
+        for(int i=1;i<n;i++){
+            if(v[i-1]-i>0 && m[v[i-1]-i]==0){
+                v.push_back(v[i-1]-i);
+                m[v[i-1]-i]=1;
+            }
+            else{
+                v.push_back(v[i-1]+i);
+                m[v[i-1]+i]=1;
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        vector<int> ans = ob.recamanSequence(n);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
