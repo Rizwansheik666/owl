fbf222ec9bfd96b7804053d2cb01df5bf1a8329c
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-06-27 11:58:25 +0530

    Time: 212 ms (12.10%) | Memory: 87 MB (20.90%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,3 +1,9 @@
+static const auto _ = [](){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    return 0;
+}();
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
