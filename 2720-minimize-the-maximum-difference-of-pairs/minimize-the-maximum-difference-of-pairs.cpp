7d14f4ad0a4fb5be7be111544b7c67ad2f831e97
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-07-03 15:53:01 +0530

    Time: 123 ms (11.62%) | Memory: 85.5 MB (75.93%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        
+        int left = 0, right = nums.back() - nums.front();
+        // cout<<right<<" ";
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            cout<<mid;
+            if (canFormPairs(nums, mid, p)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    bool canFormPairs(const vector<int>& nums, int maxDiff, int p) {
+        int pairs = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i + 1] - nums[i] <= maxDiff) {
+                pairs++;
+                i++;
+            }
+            if (pairs >= p) return true;
+        }
+        return pairs >= p;
+    }
+};
