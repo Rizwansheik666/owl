5b265979d40d17395dc20cce994dba868781d811
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-07-22 14:41:51 +0530

    Time: 6 ms (59.03%) | Memory: 14.6 MB (78.64%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
         map<int,vector<int>>mpp;
-        for(auto it:mp){
-            mpp[it.second].push_back(it.first);
-        }
         vector<int>ans;
+        for(int i=0;i<nums.size();i++)mp[nums[i]]++;
+        for(auto it:mp)mpp[it.second].push_back(it.first);
         for(auto it:mpp){
             for(int i=it.second.size()-1;i>=0;i--){
                 for(int j=0;j<it.first;j++){
