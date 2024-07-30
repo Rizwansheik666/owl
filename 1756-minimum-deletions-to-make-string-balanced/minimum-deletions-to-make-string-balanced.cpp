ba20b726acaaa61d0ccfd08e55ab94a6246161d0
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-07-30 23:22:52 +0530

    Time: 77 ms (87.56%) | Memory: 23.7 MB (66.99%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int cnt=0,ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='b'){
+                cnt++;
+            }
+            else if(cnt){
+                ans++;
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
