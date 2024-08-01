12be14365a1254a2df310d6177b4419e13e90a88
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2024-08-01 11:17:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        // code here
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>ans;
+        
+        int top=0,down=n-1;
+        int left=0,right=m-1;
+        
+        while(left<=right and top<=down){
+            
+            for(int i=left;i<=right;i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=down;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=down){
+                for(int i=right;i>=left;i--){
+                    ans.push_back(matrix[down][i]);
+                }
+                down--;
+            }
+            if(left<=right){
+            for(int i=down;i>=top;i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
