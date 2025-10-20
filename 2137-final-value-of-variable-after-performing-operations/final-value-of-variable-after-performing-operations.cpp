e095a07a7022c7884dbbb9eda406f91765e216c4
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2025-10-20 11:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (10.37%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -1,3 +1,10 @@
+static const auto _ = [](){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    return 0;
+}();
+
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
