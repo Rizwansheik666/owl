fc486843af4eddbae4a4623a47c6f724873dc281
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2026-05-22 17:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -8,12 +8,12 @@ public:
       if(nums[m]==target)
         return m;
       if(nums[l]<=nums[m]) {  
-        if (nums[l]<=target && target<nums[m])
+        if (nums[l]<=target and target<nums[m])
           r=m-1;
         else
           l= m+1;
       } else{  
-        if(nums[m]<target && target<=nums[r])
+        if(nums[m]<target and target<=nums[r])
           l=m+1;
         else
           r=m-1;
