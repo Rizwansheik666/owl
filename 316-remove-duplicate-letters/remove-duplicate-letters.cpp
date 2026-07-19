80e2a2c88d30ad310188c25db17fec977f9599ab
RIZWAN <98603170+Rizwansheik666@users.noreply.github.com>
2026-07-19 17:07:28 +0530

    Time: 3 ms (16.72%) | Memory: 9.2 MB (68.83%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        map<char,int>mp;
+        vector<bool>inAns(26,false);
+        string ans="";
+        for(auto l:s){
+            mp[l]++;
+        }
+        for (char c : s) {
+            while (!inAns[c - 'a'] &&
+                   !ans.empty() &&
+                   ans.back() > c &&
+                   mp[ans.back()] > 0) {
+                inAns[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+            mp[c]--;
+            if (!inAns[c - 'a']) {
+                inAns[c - 'a'] = true;
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+};
